858fd78cec76ebc177d189f5be04a81721895d5a
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-05-24 08:53:49 +0700

    Add files via upload

---
+++ abstract.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_5._1
+{
+    using System;
+
+    abstract class sieunhan
+    {
+        public abstract void bienhinh();
+
+        public void xuathien()
+        {
+            Console.WriteLine("Sieunhan den!");
+        }
+    }
+    class SieunhanGao : sieunhan
+    {
+        public override void bienhinh()
+        {
+            Console.WriteLine("Rut kiem va het");
+        }
+
+    }
+    class SieunhanCuongphong : sieunhan
+    {
+        public override void bienhinh()
+        {
+            Console.WriteLine("Rut o va het");
+        }
+
+    }
+    class test
+    {
+        static void Main(string[] args)
+        {
+            sieunhan SieunhanGao = new SieunhanGao();
+            SieunhanGao.bienhinh();
+            SieunhanGao.xuathien();
+
+            sieunhan SieunhanCuongphong = new SieunhanCuongphong();
+            SieunhanCuongphong.bienhinh();
+            SieunhanCuongphong.xuathien();
+        }
+    }
+
+
+}
